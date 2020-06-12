1a6142abfc3a6d64abc583744e937bb3577271bb
faisalamruddin <54799425+faisalamruddin@users.noreply.github.com>
2020-06-12 19:47:13 +0700

    Create init.js

---
+++ test/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC9K-JKtikMHuMZDRtDZnJxMw-FvZfGmsE",
+  authDomain: "mybasicproject-flamrd.firebaseapp.com",
+  databaseURL: "https://mybasicproject-flamrd.firebaseio.com",
+  projectId: "mybasicproject-flamrd",
+  storageBucket: "mybasicproject-flamrd.appspot.com",
+  messagingSenderId: "542994657421",
+  appId: "1:542994657421:web:9805b010bc6f29e454b2c4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
